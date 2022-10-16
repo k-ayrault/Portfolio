4da3332a6b16bc3f467b13ffc1dd9c68a3d61b50
k-ayrault <63095040+k-ayrault@users.noreply.github.com>
2022-10-16 17:26:11 +0200

    Routing + correction header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AccueilComponent,
+    PresentationComponent,
+    ProjetsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
